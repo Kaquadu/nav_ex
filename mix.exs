d385821a4b7a57c76cd405f43a221e95cdf525a2
kuba-kowalczykowski-lli <kuba.kowalczykowski@llinformatics.com>
2023-05-23 19:04:55 +0200

    Files fix

--- mix.exs
+++ mix.exs
@@ -46,8 +46,7 @@ defmodule NavEx.MixProject do
 
   defp package() do
     [
-      files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
-                license* CHANGELOG* changelog* src),
+      files: ~w(lib .formatter.exs mix.exs README* LICENSE*),
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/Kaquadu/nav_ex"}
     ]
